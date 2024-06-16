0edae98b1b5d33c6ef79376cd1515c1680184fc6
klaas-be <mail@klaas-behrendt.de>
2024-06-16 16:03:56 +0200

    ADDED utterance to 404 pages

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -1,5 +1,6 @@
 import { i18n } from "../../i18n"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
+import Comments from "../Comments";
 
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   // If baseUrl contains a pathname after the domain, use this as the home link
@@ -7,12 +8,24 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   const baseDir = url.pathname
 
   return (
-    <article class="popover-hint">
-      <h1>404</h1>
-      <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
-    </article>
+      <article class="popover-hint">
+          <h1>404</h1>
+          <p>{i18n(cfg.locale).pages.error.notFound}</p>
+          <p> It looks like I haven't written this page yet. If you are interested in this topic, please let me know by
+              leaving a comment!</p>
+          <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
+          <script src="https://utteranc.es/client.js"
+                  repo="klaas-be/DigitalGarden"
+                  issue-term="pathname"
+                  label="Comment"
+                  theme="github-dark"
+                  crossOrigin="anonymous"
+                  async>
+          </script>
+      </article>
+
   )
 }
 
+//<Comments></Comments>
 export default (() => NotFound) satisfies QuartzComponentConstructor
